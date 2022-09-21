de3660eb2e6776f1a28d82342dc58cef21d4d455
redwanrawbot0 <113873723+redwanrawbot0@users.noreply.github.com>
2022-09-21 03:53:29 +0300

    Update RUNFA.sh

--- RUNFA.sh
+++ RUNFA.sh
@@ -20,7 +20,7 @@ echo -e "\e[38;5;77m       •{ install source Faeder [30%]  }•\e[0m"
 if [ "$Version" == "18" ]; then
 echo -e "\033[0;31m\n~ Installing Depedencies For Ubuntu 18... \n\033[0m"
 cd /lib/x86_64-linux-gnu/ && sudo ln -s libreadline.so.7.0 libreadline.so.6
-wget "ibotcorp.com/files/compat-libevent2-5_2.0.21-1ubuntu18_amd64.deb" && sudo dpkg -i compat-libevent2-5_2.0.21-1ubuntu18_amd64.deb
+wget sudo dpkg -i compat-libevent2-5_2.0.21-1ubuntu18_amd64.deb
 rm compat-libevent2-5_2.0.21-1ubuntu18_amd64.deb
 fi
 echo -e "\033[0;31m\n~ Depedencies For Ubuntu 18 has been installed \n\033[0m"
